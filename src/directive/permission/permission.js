5aae960538bdd93d82fbfd3fc232adf64243e862
sunny lee <bevis.god@gmail.com>
2021-11-28 19:19:46 +0800

    1

--- src/directive/permission/permission.js
+++ src/directive/permission/permission.js
@@ -1,16 +1,16 @@
-
 import store from '@/store'
 
-export default{
+export default {
   // inserted函数：当被绑定的元素插入到 DOM 中时……
   inserted(el, binding, vnode) {
-    const { value } = binding  // 获取指令绑定的值;
+    const { value } = binding // 获取指令绑定的值;
     const roles = store.getters && store.getters.roles //用户本身的roles信息,arr;
 
     if (value && value instanceof Array && value.length > 0) {
       const permissionRoles = value
 
-      const hasPermission = roles.some(role => { // 只要有一个满足即返回true
+      const hasPermission = roles.some((role) => {
+        // 只要有一个满足即返回true
         return permissionRoles.includes(role)
       })
       // 没有该指令,直接删除掉该指令元素;即页面不显示没有指令权限的按钮;
@@ -21,6 +21,5 @@ export default{
     } else {
       throw new Error(`need roles! Like v-permission="['admin','editor']"`)
     }
-  }
+  },
 }
-
